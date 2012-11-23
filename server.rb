e24c58663c04253c328e5b45ebb217eb4bc215e4
Johan Eckerström <johan@duh.se>
2012-11-23 09:52:55 +0100

    Refactor command matching

--- server.rb
+++ server.rb
@@ -9,7 +9,7 @@ class LiuLunch < Sinatra::Base
     return response = 'Ät matlåda' unless @food_list
 
     commands = request.params['message'].strip.downcase.split(/\s+/)
-    match_commands(commands)
+    match_commands(commands) || help
   end
 
 private
@@ -22,17 +22,21 @@ private
   end
 
   def match_commands(commands)
-    case commands.first
-    when /ro{0,1}u{0,1}l{1,2}et{1,2}e/
+    command = commands.first
+    if /ro{0,1}u{0,1}l{1,2}et{1,2}e/.match(command)
       roulette(commands)
-    else
-      menu = @food_list.select {|d| d[:restaurant].downcase.strip == commands.join }
-      unless menu.empty?
-        menu.collect {|row| "* #{row[:food].strip}" }.join("\n")
-      else
-        help
-      end
+    elsif restaurants.include?(command)
+      menu_for(command)
     end
+  end
+
+  def menu_for(restaurant)
+    menu = @food_list.select { |f| f[:restaurant].downcase.strip == restaurant }
+    menu.map { |r| "* #{r[:food].strip}" }.join("\n")
+  end
+
+  def restaurants
+    @food_list.map { |f| f[:restaurant].downcase.strip }.uniq
   end
 
   def roulette(commands)
